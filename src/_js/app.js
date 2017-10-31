28027b3bf23814dd587af485c06e6783d57283d8
atmccann <allisontmccann@gmail.com>
2017-10-31 15:50:10 -0400

    add webfonts

--- src/_js/app.js
+++ src/_js/app.js
@@ -1,29 +1,29 @@
-'use strict';
+// 'use strict';
 
-//
-// app.js is the entry point for the entire client-side
-// application. All of the top-level views should be
-// instantiated here.
+// //
+// // app.js is the entry point for the entire client-side
+// // application. All of the top-level views should be
+// // instantiated here.
 
-var utils = require('./utils.js');
-var _ = require('underscore');
-var d3 = require('d3');
+// var utils = require('./utils.js');
+// var _ = require('underscore');
+// var d3 = require('d3');
 
-//data and chart
-var scatterChart = require('./chart.js');
-var data = require("../_data/artists.json");
+// //data and chart
+// var scatterChart = require('./chart.js');
+// var data = require("../_data/artists.json");
 
 
-var scatterCharts = [],
-    chart;
+// var scatterCharts = [],
+//     chart;
 
-function drawScatterCharts() {
+// function drawScatterCharts() {
 
-    var chart = new scatterChart();
-    chart.init("#scatter-container", data);
-    scatterCharts.push(chart);
+//     var chart = new scatterChart();
+//     chart.init("#scatter-container", data);
+//     scatterCharts.push(chart);
 
-}
+// }
 
-drawScatterCharts();
+// drawScatterCharts();
 
